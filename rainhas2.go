f0d4997128936abdf3dbb2d2fbdd4b9653a2f476
Marcus <88283829+PyMarcus@users.noreply.github.com>
2024-03-13 02:00:43 -0300

    Update rainhas2.go

--- rainhas2.go
+++ rainhas2.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-const LINE int = 4
-const COLUMN int = 4
+const LINE int = 8
+const COLUMN int = 8
 
 func createMatrix(matrix *[][]int) {
 	for i := 0; i < COLUMN; i++ {
@@ -59,13 +59,50 @@ func checkLine(matrix *[][]int, posY int) {
 		}
 	}
 
+}
+
+func checkDiagonal(matrix *[][]int, posX, posY int) {
+	validator := false
+	refMain := posX - posY
+	//sum := refMain + 2
+	for i := 0; i < LINE; i++ {
+		for j := 0; j < COLUMN; j++ {
+			if (refMain == (i-j) && (*matrix)[i][j] == 8) || (posX-posY == (i-j) && (*matrix)[i][j] == 8 && i != posX && j != posY) || (posX+posY == (i+j) && (*matrix)[i][j] == 8 && i != posX && j != posY) {
+				validator = true
+				break
+			}
+			//sum += 2
+		}
+	}
+	//sum = refMain + 2
+	fck := posX + posY
+	if validator {
+		for i := 0; i < LINE; i++ {
+			for j := 0; j < COLUMN; j++ {
+				if refMain == (i-j) && (*matrix)[i][j] != 8 {
+					(*matrix)[i][j] = 1
+				}
+				if posX-posY == (i-j) && (*matrix)[i][j] != 8 && i != posX && j != posY {
+					(*matrix)[i][j] = 1
+					//sum += 2
+				}
+				if fck == (i+j) && (*matrix)[i][j] != 8 && i != posX && j != posY {
+					(*matrix)[i][j] = 1
+
+				}
+			}
+
+		}
+
+	}
 	printMatrix(matrix)
 }
 
 func main() {
 	matrix := make([][]int, LINE)
 	createMatrix(&matrix)
-	matrix[0][2] = 8
-	checkColumn(&matrix, 0)
-	checkLine(&matrix, 2)
+	matrix[3][3] = 8
+	checkColumn(&matrix, 3)
+	checkLine(&matrix, 3)
+	checkDiagonal(&matrix, 3, 3)
 }
